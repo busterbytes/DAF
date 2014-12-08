4a0abe8ea75be34ed4c66a5896cb932920897aa5
Shane <buster@busterbytes.com>
2014-12-08 01:14:51 -0600

    donefortonight

--- 50K.R
+++ 50K.R
@@ -1,7 +1,7 @@
 library(FNN)
 training = read.csv("50Ktrain.csv")
-test = read.csv("50Ktest.csv")
+test = read.csv("50Ktest2.csv")
 cl = read.csv("cl.csv")
 cl = cl[,1]
-knn(training, test, cl, k = 3, prob=TRUE)
-attributes(.Last.value)
+knn(training, test, cl, k = 3)#, prob=TRUE)
+#attributes(.Last.value)
